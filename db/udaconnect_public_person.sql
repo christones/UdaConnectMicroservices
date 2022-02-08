791bb657ecc6e7b86e498922e9fca367ca1d7bfe
christones <elobochris@gmail.com>
2022-02-08 07:04:29 +0100

    First commit

---
+++ db/udaconnect_public_person.sql
@@ -0,0 +1,9 @@
+insert into public.person (id, first_name, last_name, company_name) values (5, 'Taco', 'Fargo', 'Alpha Omega Upholstery');
+insert into public.person (id, first_name, last_name, company_name) values (6, 'Frank', 'Shader', 'USDA');
+insert into public.person (id, first_name, last_name, company_name) values (1, 'Pam', 'Trexler', 'Hampton, Hampton and McQuill');
+insert into public.person (id, first_name, last_name, company_name) values (8, 'Paul', 'Badman', 'Paul Badman & Associates');
+insert into public.person (id, first_name, last_name, company_name) values (9, 'Otto', 'Spring', 'The Chicken Sisters Restaurant');
+insert into public.person (id, first_name, last_name, company_name) values (10, 'Elobo', 'Christian', 'Tchollire Restaurant');
+insert into public.person (id, first_name, last_name, company_name) values (11, 'Tsona', 'Dodo', 'Zingana Hotel');
+insert into public.person (id, first_name, last_name, company_name) values (12, 'Ekwalla', 'Flaubert', 'Choco Cho Bonapriso');
+insert into public.person (id, first_name, last_name, company_name) values (13, 'Thomas', 'Anaba', 'Hotel le Paradis');
