b9dde7bdbb2102014e79c2d199d791854ecd065c
christones <elobochris@gmail.com>
2022-02-19 14:53:00 +0100

    Complete UdaFinal Connect

--- modules/03-locationmicroservice1/location-event-client-grpc.py
+++ modules/03-locationmicroservice1/location-event-client-grpc.py
@@ -14,20 +14,34 @@ stub = coordinates_event_pb2_grpc.ItemServiceStub(channel)
 
 # Update this with desired payload
 user_coordinates = coordinates_event_pb2.EventCoordinatesMessage(
-    userId=300,
-    latitude=-100,
-    longitude=30
+    userId=340,
+    latitude=-180,
+    longitude=125
 )
 
 user_coordinates_2 = coordinates_event_pb2.EventCoordinatesMessage(
-    userId=400,
-    latitude=-100,
-    longitude=30
+    userId=402,
+    latitude=-156,
+    longitude=39
+)
+
+user_coordinates_3 = coordinates_event_pb2.EventCoordinatesMessage(
+    userId=150,
+    latitude=-200,
+    longitude=89
+)
+
+user_coordinates_4 = coordinates_event_pb2.EventCoordinatesMessage(
+    userId=100,
+    latitude=-200,
+    longitude=300
 )
 
 response_1 = stub.Create(user_coordinates)
 response_2 = stub.Create(user_coordinates_2)
+response_3 = stub.Create(user_coordinates_3)
+response_4 = stub.Create(user_coordinates_4)
 
 
 print("Coordinates sent...")
-print(user_coordinates, user_coordinates_2)
+print(user_coordinates, user_coordinates_2, user_coordinates_3, user_coordinates_4 )
